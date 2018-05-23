697f5a39ed27e19c8fb197d69b9ccc7fafdc6759
pollockcl <pollock.clee@gmail.com>
2018-05-23 16:30:36 -0600

    devise did more than I thought it did. Undid the damage I caused.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, presence: true, uniqueness: true
 end
