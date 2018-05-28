1f62e451bbc6281052a4e6f97e632b75f4d616d9
pollockcl <pollock.clee@gmail.com>
2018-05-28 16:30:07 -0600

    built out the search function

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,9 @@
 class CardsController < ApplicationController
   before_action :set_card, only: :show
+  before_action :search, only: :index
 
   def index
-    @cards = Card.paginate(page: params[:page], per_page: 25)
+    @cards.paginate(page: params[:page], per_page: 25)
   end
 
   def show
@@ -13,5 +14,9 @@ class CardsController < ApplicationController
     
   def set_card
     @card = Card.find_by(name: params[:slug])
+  end
+
+  def search
+    @cards = Card.where("name LIKE '%#{params[:search][:q]}%'")
   end
 end
