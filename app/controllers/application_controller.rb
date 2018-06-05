7c4efe7ccbb59b5842bbb4a3f839acf19bd7c9fb
pollockcl <pollock.clee@gmail.com>
2018-06-05 16:14:55 -0600

    tracking page load times

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_inventory
   helper_method :current_total
+  helper_method :set_start_time
+
+  before_action :set_start_time
+
+  def set_start_time
+    @start_time = Time.now.to_f
+  end
 
   def current_inventory
     @current_inventory ||= current_user.inventories.first
@@ -14,3 +21,4 @@ class ApplicationController < ActionController::Base
     end.sum
   end
 end
+
