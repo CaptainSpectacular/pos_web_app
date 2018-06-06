1fdfeddb0142439d4a77b7921b5d56128805be4c
pollockcl <pollock.clee@gmail.com>
2018-06-06 14:22:06 -0600

    fixed error causing the session to not be set on registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :set_transaction, only: [:create]
   private
 
   def sign_up_params
     params.require(:user).permit(:email, :password, :password_confirmation, :company)
+  end
+
+  def set_transaction
+    session[:current_transaction] = Hash.new
   end
 end
